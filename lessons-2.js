68db3def9d686b6510386472d7128920588a3bbf
unknown <ggvp2017uwu@gmail.com>
2024-12-11 18:52:20 +0200

    Доробив друге завдання

--- lessons-2.js
+++ lessons-2.js
@@ -11,3 +11,63 @@ const hoursInMonth = daysInMonth * 24;
 const minutesInMonth = 60 * hoursInMonth;
 
 console.log(minutesInMonth);
+
+let health = 100;
+
+let stamina = 100;
+
+health = health - 10;
+
+stamina -= 10;
+
+console.log(health, stamina);
+
+const totalPrice = 1200;
+
+const discount = 0.1;
+
+const discountedPrice = totalPirce * discount;
+
+console.log(discount);
+
+console.log(totalPrice);
+
+console.log(discountedPrice);
+
+const FloatNumber = 12.78;
+
+const roundedDown = Math.floor(FloatNumber);
+
+console.log(FloatNumber);
+
+console.log(roundedDown);
+
+const floatString = "123.45";
+
+const decimalNumber = floatString;
+
+console.log(decimalNumber);
+
+const integerString = "123";
+
+const wholeNumber = parseInt(integerString);
+
+console.log(wholeNumber);
+
+const number = 24;
+
+const root = Math.sqrt(number);
+
+console.log(root);
+
+const integerNumber = 102;
+
+const Stringnumber = 328;
+
+const parsedNumber = parseInt(Stringnumber);
+
+console.log(integerNumber);
+
+const stringifiedNumber = integerNumber.toString;
+
+console.log(stringifiedNumber);
