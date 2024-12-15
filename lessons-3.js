d8b2ae97dec0fb9e9054abb86c62ec2d3311ac3d
unknown <ggvp2017uwu@gmail.com>
2024-12-15 17:51:47 +0200

    Зробив третье завдання у вкладцы lessons-3

---
+++ lessons-3.js
@@ -0,0 +1,24 @@
+const result = 5 + 5 + 5;
+
+console.log(result);
+
+const email = "zhelukarseniy@gmail.com";
+
+const containsAtsimbol = email.includes("@");
+
+console.log(containsAtsimbol);
+
+const Lattery1 = "14";
+
+const Lattery2 = "Arseniy";
+
+const Lattery3 = "Is";
+
+const fulllattery = Lattery1 + "14" + Lattery2 + "Arseniy" + Lattery3;
+
+const fulllattery += "Arseniy"
+
+const Nameoleksandr = "Олександр";
+
+const cash = "300" ;
+alert(Дякуємо, Олександр до сплати 300 грн)
