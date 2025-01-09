b62f8b1a7d998de8fea91b53bb6473a6a4d03aee
unknown <ggvp2017uwu@gmail.com>
2025-01-09 18:25:41 +0200

    Зробив 5 завданяя

---
+++ lessons-5.js
@@ -0,0 +1,66 @@
+/* Завдання 1 */
+const drink = "Coffee";
+
+switch (drink) {
+  case "Coffee":
+    console.log("Coffee selected");
+    break;
+  case "Tea":
+    console.log("Tea selected");
+    break;
+  case "Juice":
+    console.log("Juice selected");
+    break;
+  default:
+    console.log("Unknown drink");
+}
+
+/* Завдання 2 */
+
+const dayOfWeek = "Thursday";
+
+if (
+  ["Monday" && "Thuesday" && "Wednsdey" && "Thursday" && "Friday"].includes(
+    dayOfWeek
+  )
+) {
+  console.log("Це робочий день");
+} else if (["Saturday", "Sunday"].includes(dayOfWeek)) {
+  console.log("Unknown day");
+} else {
+  console.log("Weekend day");
+}
+
+/* Завдання 3 */
+
+const monthNumber = 1; // замініть на потрібний номер місяця (1-12)
+
+if ([12, 1, 2].includes(monthNumber)) {
+  console.log("Winter");
+} else if ([3, 4, 5].includes(monthNumber)) {
+  console.log("Spring");
+} else if ([6, 7, 8].includes(monthNumber)) {
+  console.log("Summer");
+} else if ([9, 10, 11].includes(monthNumber)) {
+  console.log("Autumn");
+} else {
+  console.log("Unknownmonth");
+}
+
+/* Завдання 4 */
+
+const color = "Red";
+
+switch (color) {
+  case "Red":
+    console.log("Stop");
+    break;
+  case "Green":
+    console.log("Go");
+    break;
+  case "Yellow":
+    console.log("Wait");
+    break;
+  default:
+    console.log("Unknown color");
+}
