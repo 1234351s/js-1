4d438fe7c134adf2d700555a9807ba2659817d45
unknown <ggvp2017uwu@gmail.com>
2025-01-30 19:47:28 +0200

    Update lessons-7.js

--- lessons-7.js
+++ lessons-7.js
@@ -1,67 +1,75 @@
-// // 1 завдання
-// const numbers = [1, 2, 3];
-// function changeSecondElement(arr) {
-//   const newArr = [...arr];
-//   newArr[1] = 10;
-//   return newArr;
-// }
+// 1 завдання
 
-// const updatedNumbers = changeSecondElement(numbers);
-// console.log(updatedNumbers);
-// // 2 завдання
-// const strings = ["apple", "banana", "cherry"];
+const numbers = [1, 2, 3];
+const changeSecondElement = function (arr) {
+  const newArr = [...arr];
+  newArr[1] = 10;
+  return newArr;
+};
 
-// function addString(arr, newString) {
-//   return [...arr, newString];
-// }
+const updatedNumbers = changeSecondElement(numbers);
+console.log(updatedNumbers);
 
-// const updatedStrings = addString(strings, "date");
-// console.log(updatedStrings);
-// // 3 завдання
-// const numbers = [1, 2, 3, 4, 5];
-// function sumArray(arr) {
-//   return arr.reduce((total, num) => total + num, 0);
-// }
-// const sum = sumArray(numbers);
-// console.log(sum);
-// // 4 завдання
-// const numbers = [1, 2, 3, 4, 5];
-// function printArray(arr) {
-//   for (const num of arr) {
-//     console.log(num);
-//   }
-// }
-// printArray(numbers);
-// // 5 завдання
-// const numbers = [1, 2, 3, 4, 5];
+// 2 завдання
 
-// function printArray(arr) {
-//   for (const num of arr) {
-//     console.log(num);
-//   }
-// }
+const strings = ["apple", "banana", "cherry"];
+const addString = function (arr, newString) {
+  return [...arr, newString];
+};
 
-// printArray(numbers);
-// // 6 завдання
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const updatedStrings = addString(strings, "date");
+console.log(updatedStrings);
 
-// function findMax(arr) {
-//   return Math.max(...arr);
-// }
+// 3 завдання
 
-// const max = findMax(numbers);
-// console.log(max);
-// // 7 завдання
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers = [1, 2, 3, 4, 5];
+const sumArray = function (arr) {
+  return arr.reduce((total, num) => total + num, 0);
+};
 
-// function printEvenNumbers(arr) {
-//   for (const num of arr) {
-//     if (num % 2 === 0) {
-//       console.log(num);
-//     }
-//   }
-// }
+const sum = sumArray(numbers);
+console.log(sum);
 
-// printEvenNumbers(numbers);
+// 4 завдання
 
-// // Попробував зробити 7-ме завдання через функціі якщо не вірно я перероблю
+const numbers = [1, 2, 3, 4, 5];
+const printArray = function (arr) {
+  for (const num of arr) {
+    console.log(num);
+  }
+};
+
+printArray(numbers);
+
+// 5 завдання
+
+const numbers = [1, 2, 3, 4, 5];
+const printArray = function (arr) {
+  for (const num of arr) {
+    console.log(num);
+  }
+};
+
+printArray(numbers);
+
+// 6 завдання
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const findMax = function (arr) {
+  return Math.max(...arr);
+};
+
+const max = findMax(numbers);
+console.log(max);
+
+// 7 завдання
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const findMax = function (arr) {
+  return Math.max(...arr);
+};
+
+const max = findMax(numbers);
+console.log(max);
+
+// Попробував зробити 7-ме завдання через функціі якщо не вірно я перероблю
