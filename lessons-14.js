691649c76997cf2f87cf2c18cca89c5bf14e80ee
1234351s <156699518+1234351s@users.noreply.github.com>
2025-03-16 23:36:08 +0200

    lessons-15

--- lessons-14.js
+++ lessons-14.js
@@ -1,33 +1,31 @@
-// 1 завдання 
-const getUserNames = users => {
-    return users.map(user => user.name);
-  };
+// // 1 завдання 
+// const getUserNames = users => {
+//     return users.map(user => user.name);
+//   };
   
-  console.log(getUserNames(users));
-// 2 завдання
-  const getUsersWithEyeColor = (users, color) => {
-    return users.filter(user => user.eyeColor === color);
-  };
+//   console.log(getUserNames(users));
+// // 2 завдання
+//   const getUsersWithEyeColor = (users, color) => {
+//     return users.filter(user => user.eyeColor === color);
+//   };
   
-  console.log(getUsersWithEyeColor(users, 'blue'));
-//  3 завдання
-  const getUsersWithGender = (users, gender) => {
-    return users.filter(user => user.gender === gender).map(user => user.name);
-  };
+//   console.log(getUsersWithEyeColor(users, 'blue'));
+// //  3 завдання
+//   const getUsersWithGender = (users, gender) => {
+//     return users.filter(user => user.gender === gender).map(user => user.name);
+//   };
   
-  console.log(getUsersWithGender(users, 'male'));
-// 4 завдання
-  const getInactiveUsers = users => {
-    return users.filter(user => !user.isActive);
-  };
+//   console.log(getUsersWithGender(users, 'male'));
+// // 4 завдання
+//   const getInactiveUsers = users => {
+//     return users.filter(user => !user.isActive);
+//   };
   
-  console.log(getInactiveUsers(users));
-// завдання 5 
-  const getUserWithEmail = (users, email) => {
-    return users.find(user => user.email === email);
-  };
+//   console.log(getInactiveUsers(users));
+// // завдання 5 
+//   const getUserWithEmail = (users, email) => {
+//     return users.find(user => user.email === email);
+//   };
   
-  console.log(getUserWithEmail(users, 'shereeanthony@kog.com'));
-  console.log(getUserWithEmail(users, 'elmahead@omatom.com'));
-
-          
+//   console.log(getUserWithEmail(users, 'shereeanthony@kog.com'));
+//   console.log(getUserWithEmail(users, 'elmahead@omatom.com'));
