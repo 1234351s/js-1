eba0c931b34f050b7d8afb6fa39da62c01f1852a
1234351s <156699518+1234351s@users.noreply.github.com>
2025-04-02 18:01:45 +0300

    Зробив 17 завдання

--- lessons-15.js
+++ lessons-15.js
@@ -1,22 +1,22 @@
-// 1 завдання 
-const calculateTotalBalance = users => {
-    return users.reduce((total, user) => total + user.balance, 0);
-  };
-//   2 завдання
-const getUsersWithFriend = (users, friendName) => {
-    return users
-      .filter(user => user.friends.includes(friendName))
-      .map(user => user.name);
-  };
+// // 1 завдання 
+// const calculateTotalBalance = users => {
+//     return users.reduce((total, user) => total + user.balance, 0);
+//   };
+// //   2 завдання
+// const getUsersWithFriend = (users, friendName) => {
+//     return users
+//       .filter(user => user.friends.includes(friendName))
+//       .map(user => user.name);
+//   };
 
-//   3 завдання
-const getNamesSortedByFriendsCount = users => {
-    return [...users]
-      .sort((a, b) => a.friends.length - b.friends.length)
-      .map(user => user.name);
-  };
-// 4 завдання 
-const getSortedUniqueSkills = users => {
-    return [...new Set(users.flatMap(user => user.skills))].sort();
-  };
+// //   3 завдання
+// const getNamesSortedByFriendsCount = users => {
+//     return [...users]
+//       .sort((a, b) => a.friends.length - b.friends.length)
+//       .map(user => user.name);
+//   };
+// // 4 завдання 
+// const getSortedUniqueSkills = users => {
+//     return [...new Set(users.flatMap(user => user.skills))].sort();
+//   };
   
---
+++ lessons-17.js
@@ -0,0 +1,18 @@
+// // Створити розмітку з кнопкою та текстовим полем. За допомогою JavaScript отримати доступ до кнопки та текстового поля за їх ідентифікаторами та змінити текст на кнопці на значення текстового поля.
+// const heading = document.getElementById('tit le');
+
+
+// heading.textContent = 'Привіт, DOM!';
+// console.log(heading)
+// // Створити розмітку з заголовком та зображенням. За допомогою JavaScript отримати доступ до зображення та змінити значення атрибута "src" на шлях до іншого зображення.
+// const mainImage = document.getElementById("mainImage");
+// Image.src = "#";
+// console.log(mainImage)
+// // Створити розмітку з посиланням та зображенням. За допомогою JavaScript отримати доступ до посилання та змінити значення атрибута "href" на нову URL-адресу. Також отримати доступ до зображення та додати новий атрибут "alt" з описом зображення.
+// const link = document.getElementById("myLink");
+// const image = document.getElementById("MyImage")
+// console.log(link)
+// console.log(image)
+// // Створити розмітку зі списком елементів. За допомогою JavaScript отримати доступ до першого елемента списку та змінити його вміст на новий текст.
+// const firstitem = document.getElementsByClassName("myList")
+// console.log(firstitem)
